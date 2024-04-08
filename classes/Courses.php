4c5353c087872ff43e2f34f127bc921a785617a6
Khemu <alghost2004@gmail.com>
2024-04-08 16:31:46 +0200

    updated

--- classes/Courses.php
+++ classes/Courses.php
@@ -4,36 +4,6 @@ class Courses
 {
 
   public static $table = "course";
-<<<<<<< HEAD
-  public static $columns = ["name", "level", "hours", "teacherAccountid"];
-  /**
-   * Inserts a new record into the course table
-   * - The method only accepts 1D array
-   * - The entered values of the array must be in this format ->  name , level , hours , teacher's account id
-   * - This method must be called along the Teacher's method
-   * @return bool true if the record was inserted, false otherwise
-   */
-  public static function insert(array $data): bool
-  {
-    if (count($data) != count(self::$columns)) {
-      echo "invalid number of parameters:";
-      return false;
-    }
-    $arr = array_combine(self::$columns, $data); // making the associative array
-
-    return DB::insert(self::$table, $arr);
-  }
-  public static function select(array $data): array
-  {
-
-
-    return DB::select(self::$table, $data);
-  }
-  public static function selectAll(array $data, array $columns)
-  {
-    return DB::selectAll(self::$table, $columns, $data);
-  }
-=======
   public static $columns = ["name", "level", "hours"];
 
 
@@ -50,5 +20,4 @@ public static function select(array $columns, array $data): array
 {
     return DB::select(self::$table, $columns, $data);
 }
->>>>>>> 6267822f74bd8f51b82a21acf7c9fedcaae04622
 }
--- classes/Students.php
+++ classes/Students.php
@@ -5,14 +5,6 @@ class Students
 {
   public static $table = "student";
   public static $columns = ["accountId", "userName", "name", "address", "level"];
-<<<<<<< HEAD
-
-
-  /**
-   * Inserts a new record into the student table
-   * - Accepts a 1D array
-   * - The entered values of the array must be in this format -> accountId, userName, name, address , level
-=======
 
   /**
    * Returns all the rows of the table
@@ -38,7 +30,6 @@ public static function select(array $columns, array $data): array
   /**
    * Inserts a new record into the Students table
    * - the entered values of the array must be in this format -> accountId, userName, name, address,level
->>>>>>> 6267822f74bd8f51b82a21acf7c9fedcaae04622
    * @param array $data an  array containing the values
    * @return bool true if the record was inserted, false otherwise
    */
@@ -51,16 +42,5 @@ public static function select(array $columns, array $data): array
     $arr = array_combine(self::$columns, $data); // making the associative array
 
     return DB::insert(self::$table, $arr);
-  }
-
-  public static function select(array $data): array
-  {
-
-
-    return DB::select(self::$table, $data);
-  }
-  public static function selectAll(array $data, array $columns)
-  {
-    return DB::selectAll(self::$table, $columns, $data);
   }
 }
