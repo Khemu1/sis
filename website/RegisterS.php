6d38b73a770dec75b4e2b2fb3f4e71477cdf9196
omartamer630 <omartamerdev@gmail.com>
2024-04-09 13:45:13 +0200

    Container RegestertionStudent Added

---
+++ website/RegisterS.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="RegisterS.css">
+  <title>EELU Sign Up</title>
+</head>
+<body>
+
+ <div class="container" width="50" height="50"align="center">
+  <div class="head"><h2 >SignUp</h2>
+    </div>
+  <div class="body" width="200" height="200">
+  <input type="text" name="userName" value="UserName" ><br>
+  <input type="text" name="name" value="name" ><br>
+  <input type="text" name="password" value="Password"><br>
+  <input type="text" name="address" value="address" ><br>
+   <input type="text" name="level" value="level" ><br>
+  <input type="submit" name="submit" value="Submit"><br>
+    </div>
+  <div class="footer" width="200" height="200">
+    <p style="font-size : 10px">Powerd by Kemet University</p>
+  </div>
+ </div>
+
+</body>
+</html>
+
+
+
+<?php
+
+?>
