dcd8ce94c36948efd4d37f3445ad5ab9d454da71
Khemu <alghost2004@gmail.com>
2024-04-13 21:48:09 +0200

    header

--- views/php/home.php
+++ views/php/home.php
@@ -10,7 +10,6 @@ require_once ("../../models/Teaches.php");
 require_once ("../../models/Utils.php");
 
 session_start();
-echo $_SESSION["id"];
 if (!isset($_SESSION["id"])) {
   header("Location: login.php");
   exit();
