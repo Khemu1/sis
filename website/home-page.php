6242f16f5318f5d536a955ab39de275e672f1936
FlameableAqua <abdelrhmanmagdyfahmy@gmail.com>
2024-04-16 20:32:39 +0200

    updated

--- website/home-page.php
+++ website/home-page.php
@@ -1,86 +1,123 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="../assets/css/home-page.css">
-</head>
-<body>
-<header>
-  <div class="navbar">
-    <div class="logo"><a href="#">kemet</a></div>
-    <nav class="nav-links">  <li><a href="main">Home</a></li>
-      <li><a href="about">About</a></li>
-      <li><a href="support">Support</a></li>
-      <li><a href="contact us">Contact</a></li>
-    </nav>
-    <a href="#" class="action-btn">Get Started</a>
-    <div class="toggle-btn">
-      <i class="fa-solid fa-bars"></i>
-    </div>
-  </div>
-<div class="dropdown-menu">
-   <li><a href="main">Home</a></li>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home</title>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+      integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <link rel="stylesheet" href="../assets/css/home-page.css" />
+  </head>
+  <body>
+    <nav>
+      <div class="logo"><a href="#">kemet</a></div>
+      <div class="nav-links">
+        <li><a href="main">Home</a></li>
         <li><a href="about">About</a></li>
         <li><a href="support">Support</a></li>
         <li><a href="contact us">Contact</a></li>
-        <li><a href="#" class="action-btn">Get Started</a></li>
-        <li><a href="#" class="action-btn">Log Out</a></li>
-</div>
-
-</header>
-<script>
-
-  const toggleBtn = document.querySelector('.toggle-btn');
-  const toggleBtnIcon = document.querySelector('.toggle-btn i');
-  const dropDownMenu = document.querySelector('.dropdown-menu');
-
-  toggleBtn.onclick = function() {
-    dropDownMenu.classList.toggle('open');
-    const isOpen = dropDownMenu.classList.contains('open');
-
-    toggleBtnIcon.classList = isOpen
-      ? 'fa-solid fa-xmark'
-      : 'fa-solid fa-bars'
-  }
-
-</script>
-
-  <footer>
-        <div class="footer-form">
-          <div class="footer-content">
-          <h3>Contact Us</h3>
-        <p>email: 6Cjz8@example.com</p>
-      <p>phone: 01000000000</p>
-        <p>address: kemet street 123</p>
       </div>
-      <div class="footer-content">
-        <h3>Quick Links</h3>
-        <br>
-        <ul class="list-footer">
-          <li><a href="main">Home</a></li>
-          <li><a href="about">About</a></li>
-          <li><a href="support">Support</a></li>
-          <li><a href="contact us">Contact</a></li>
-        </ul>
-      </div>
+      <a href="#" class="logout">logout</a>
+    </nav>
+
+    <section>
+    
+      <div class="about">
+          <div class=" about-container">
+            <h3  </h3>
+            <div class="img-container">
+              <img src="../assets/images/dedicated-server-bg.jpg">
+            </div>
+            <p >We understand the critical importance of data <b>security, speed, and reliability</b>. That's why we
+              leverage
+              dedicated servers to power our platform, ensuring uncompromising performance and stability. With dedicated
+              servers, we provide exclusive resources solely for the use of our system, eliminating the risks associated
+              with shared hosting environments. This approach allows us to guarantee high-speed data access, robust
+              security measures, and unparalleled uptime, providing our users with a seamless and dependable experience.
+              Our commitment to utilizing dedicated servers underscores our dedication to safeguarding your data and
+              delivering optimal performance at all times </p>
+          </div>
+          <div class=" about-container">
+            <h3 >Our Security System</h3>
+            <div class="img-container">
+              <img src="../assets/images/security.webp">
+            </div>
+            <p >we prioritize the security of our users' data above all else. Our comprehensive security system is
+              designed
+              to safeguard sensitive information and protect against potential threats. Utilizing state-of-the-art
+              encryption protocols, multi-factor authentication, and continuous monitoring, we ensure that your data
+              remains secure at all times. Our proactive approach to security includes regular security audits,
+              vulnerability assessments, and rapid response to emerging threats. With robust firewalls, intrusion
+              detection systems, and real-time threat intelligence, we mitigate risks and maintain the integrity of our
+              platform. Rest assured, your information is in safe hands with <b>Kemet</b> </p>
+          </div>
+          <div class="about-container">
+            <h3 >Stable Connection</h3>
+            <div class="img-container">
+              <img src="../assets/images/importance-of-a-reliable-internet-connection-for-business-TeleCloud.png">
+            </div>
+            <p >At <b>Kemet</b>, we understand the significance of a stable connection in today's digital age. That's why
+              we
+              prioritize ensuring a reliable and secure connection for all users. With our robust infrastructure and
+              dedicated team, we guarantee a seamless online experience, enabling students, teachers, and administrators
+              to access our platform anytime, anywhere. Our commitment to maintaining a stable connection reflects our
+              dedication to facilitating smooth communication, efficient collaboration, and uninterrupted access to
+              essential educational resources.</p>
+          </div>
+          <div class="about-container">
+            <h3 >Maintenance</h3>
+            <div class="img-container">
+              <img src="../assets/images/matintenance.png">
+            </div>
+            <p >Ensuring optimal performance and reliability of our systems is paramount at SIS. Our proactive approach
+              to
+              maintenance encompasses regular checks, updates, and fine-tuning of our infrastructure to prevent downtime
+              and minimize disruptions. With a dedicated team of experts overseeing our maintenance operations, we
+              swiftly
+              address any potential issues before they escalate, keeping our systems running smoothly. By prioritizing
+              preventative maintenance, we uphold our commitment to providing a seamless and uninterrupted user
+              experience
+              for students, teachers, and administrators alike. </p>
+          </div>
+        </div>
+    </section>
+
+    <footer>
+      <div class="footer-contents">
         <div class="footer-content">
-          <br>
-          <h3>Follow Us</h3>
+          <h5>Contact Us</h5>
+          <p>email: 6Cjz8@example.com</p>
+          <p>phone: 01000000000</p>
+          <p>address: kemet street 123</p>
+        </div>
+        <div class="footer-content">
+          <h5>Quick Links</h5>
+          <br />
+          <ul class="list-footer">
+            <li><a href="main">Home</a></li>
+            <li><a href="about">About</a></li>
+            <li><a href="support">Support</a></li>
+            <li><a href="contact us">Contact</a></li>
+          </ul>
+        </div>
+        <div class="footer-content">
+          <br />
+          <h5>Follow Us</h5>
           <ul class="socials">
-            <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+            <li>
+              <a href="#"><i class="fa-brands fa-facebook"></i></a>
+            </li>
           </ul>
         </div>
         <div class="bottom-bar">
           <p>&copy; 2024 Kemet all rights reserved</p>
         </div>
-    </div>
-  </footer>
-
-
-
-
-</body>
+      </div>
+    </footer>
+  </body>
 </html>
