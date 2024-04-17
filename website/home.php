94a01dfefe4b03cee1785cf1672f4bec07e57e27
Khemu <alghost2004@gmail.com>
2024-04-17 10:32:53 +0200

    closed the section tag in home.php

--- website/home.php
+++ website/home.php
@@ -23,66 +23,6 @@
   </nav>
 
   <section>
-
-    <!-- <div class="about">
-          <div class=" about-container">
-            <h3></h3>
-            <div class="img-container">
-              <img src="../assets/images/dedicated-server-bg.jpg">
-            </div>
-            <p >We understand the critical importance of data <b>security, speed, and reliability</b>. That's why we
-              leverage
-              dedicated servers to power our platform, ensuring uncompromising performance and stability. With dedicated
-              servers, we provide exclusive resources solely for the use of our system, eliminating the risks associated
-              with shared hosting environments. This approach allows us to guarantee high-speed data access, robust
-              security measures, and unparalleled uptime, providing our users with a seamless and dependable experience.
-              Our commitment to utilizing dedicated servers underscores our dedication to safeguarding your data and
-              delivering optimal performance at all times </p>
-          </div>
-          <div class=" about-container">
-            <h3 >Our Security System</h3>
-            <div class="img-container">
-              <img src="../assets/images/security.webp">
-            </div>
-            <p >we prioritize the security of our users' data above all else. Our comprehensive security system is
-              designed
-              to safeguard sensitive information and protect against potential threats. Utilizing state-of-the-art
-              encryption protocols, multi-factor authentication, and continuous monitoring, we ensure that your data
-              remains secure at all times. Our proactive approach to security includes regular security audits,
-              vulnerability assessments, and rapid response to emerging threats. With robust firewalls, intrusion
-              detection systems, and real-time threat intelligence, we mitigate risks and maintain the integrity of our
-              platform. Rest assured, your information is in safe hands with <b>Kemet</b> </p>
-          </div>
-          <div class="about-container">
-            <h3 >Stable Connection</h3>
-            <div class="img-container">
-              <img src="../assets/images/importance-of-a-reliable-internet-connection-for-business-TeleCloud.png">
-            </div>
-            <p >At <b>Kemet</b>, we understand the significance of a stable connection in today's digital age. That's why
-              we
-              prioritize ensuring a reliable and secure connection for all users. With our robust infrastructure and
-              dedicated team, we guarantee a seamless online experience, enabling students, teachers, and administrators
-              to access our platform anytime, anywhere. Our commitment to maintaining a stable connection reflects our
-              dedication to facilitating smooth communication, efficient collaboration, and uninterrupted access to
-              essential educational resources.</p>
-          </div>
-          <div class="about-container">
-            <h3 >Maintenance</h3>
-            <div class="img-container">
-              <img src="../assets/images/matintenance.png">
-            </div>
-            <p >Ensuring optimal performance and reliability of our systems is paramount at SIS. Our proactive approach
-              to
-              maintenance encompasses regular checks, updates, and fine-tuning of our infrastructure to prevent downtime
-              and minimize disruptions. With a dedicated team of experts overseeing our maintenance operations, we
-              swiftly
-              address any potential issues before they escalate, keeping our systems running smoothly. By prioritizing
-              preventative maintenance, we uphold our commitment to providing a seamless and uninterrupted user
-              experience
-              for students, teachers, and administrators alike. </p>
-          </div>
-        </div>
-    </section> -->
     <h2>Enrolled Students </h2>
     <div class="student-table-container">
       <div class="students">
@@ -247,6 +187,7 @@
         </div>
       </div>
     </footer>
+  </section>
 </body>
 
 </html>
