b6a703c8f3de1d3836da99ddd659edc5c065d913
NeuroBoot <91323608+NeuroBoot@users.noreply.github.com>
2024-04-17 11:48:35 +0200

    Update Teacher

--- website/TeacherRegister.php
+++ website/TeacherRegister.php
@@ -7,6 +7,7 @@ require_once ("../models/Students.php");
 require_once ("../models/Teachers.php");
 require_once ("../models/Teaches.php");
 require_once ("../models/Utils.php");
+session_start();
 $arr = [];
 if (isset($_POST["register"])) {
   $arr = Utils::validateStudentFields([
@@ -24,41 +25,48 @@ if (isset($_POST["register"])) {
     Accounts::insert($Account);
     $accountId = intval(Accounts::select(["id"], ["userName" => $userName])[0]["id"]);
     $teachers = [$accountId, $userName, $name, $address];
-    Teachers::insert($teachers);
-    Enrollment::enroll();
+    Teachers::insert($teacher);
+  
     $_SESSION["id"] = $accountId;
     $_SESSION["userName"] = $userName;
     $_SESSION["type"] = "teacher";
   }
-}?>
-<?php
 
-session_start();
-?>
+ for($i - 0;$i < count($courses);i++) 
+ {$teaches = [$userName], $course];
+  Teaches::insert($teaches);}
+  // saving the courses of teachers
+ Enrollment::enroll();
+
+}?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="TeacherRegister.css">
-  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+  <link rel="stylesheet" href="../assets/css/TeacherRegister.css">
+  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">  
   <title>Kemet Sign Up</title>
 </head>
 
 <body>
 
-  <div class="container" width="50" height="50" align="center">
+  <div class="container" >
     <div class="head">
       <h2>SignUp</h2>
     </div>
-    <div class="body" width="200" height="200">
+    <div class="body" >
       <form method="Post">
         <input type="text" name="userName" class="user-name" placeholder="UserName"><br>
+        <?php echo $arr["userName"] ?? " "; echo $arr["account"] ?? " ";?>
         <input type="text" name="name" class="name" placeholder="name"><br>
+        <?php echo $arr["name"] ?? " ";?>
         <input type="text" name="password" class="password" placeholder="Password"><br>
+        <?php echo $arr["password"] ?? " ";?>
         <input type="text" name="address" class="address" placeholder="address"><br>
-
+        <?php echo $arr["address"] ?? " ";?>
         <div class="dropdown">
           <button>Choose courses<i class="uil uil-angle-down"></i></button>
           
@@ -113,26 +121,28 @@ session_start();
           </div>
           
        
-            
           
             </div>
-          </div>
+          </div><?php echo $arr["courses"] ?? " ";?>
+          <div class="invalid-courses hide"> </div><br>
+          <input type="submit" name="register" value="Register"><br>
+          <a class="font-dark" href="login.php">Login</a>
+  
+        </form>
+      </div>
+      <div class="footer" >
+        <p>Powerd by Kemet University</p>
+      </div>
+    </div>
         </div>
-        <div class="invalid-courses hide">Invalid number of courses : Please choose atleast one course </div>
-        <input type="submit" name="register" value="Register"><br>
-        <a class="font-dark" href="login.php">Login</a>
-
-      </form>
-    </div>
-    <div class="footer" width="200" height="200">
-      <p style="font-size : 10px">Powerd by Kemet University</p>
-    </div>
-  </div>
+        
+       
 
 </body>
 
 </html>
 <script type="module" src="../js/TeacherRegister.js?t=<?= time() ?>"></script>
+<script type="module" src="../js/Utils.js?t=<?= time() ?>"></script>
 
 
 
