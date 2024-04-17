856e6fe3ab3184cb405de783cc967bab97129c6e
Khemu <alghost2004@gmail.com>
2024-04-18 00:51:32 +0200

    updated

--- website/TeacherRegister.php
+++ website/TeacherRegister.php
@@ -15,31 +15,33 @@ if (isset($_POST["register"])) {
     $_POST["name"],
     $_POST["password"],
     $_POST["address"],
+    $_POST["course"] ?? []
   ]);
-  $userName = $_POST["userName"];
-  $name = $_POST["name"];
-  $password = $_POST["password"];
-  $address = $_POST["address"];
   if (empty($arr)) {
+    $courses = $_POST["course"];
+    $userName = $_POST["userName"];
+    $name = $_POST["name"];
+    $password = $_POST["password"];
+    $address = $_POST["address"];
     $Account = [$userName, $password];
     Accounts::insert($Account);
     $accountId = intval(Accounts::select(["id"], ["userName" => $userName])[0]["id"]);
-    $teachers = [$accountId, $userName, $name, $address];
+    $teacher = [$accountId, $userName, $name, $address];
     Teachers::insert($teacher);
-  
     $_SESSION["id"] = $accountId;
     $_SESSION["userName"] = $userName;
     $_SESSION["type"] = "teacher";
+
+    for ($i = 0; $i < count($courses); $i++) {
+      $teaches = [$userName, $courses[$i]];
+      Teaches::insert($teaches);
+    }
+    Enrollment::enroll();
+    header("location: home.php");
+    exit();
   }
-
- for($i - 0;$i < count($courses);$i++) 
- {$teaches = [$userName, $course];
-  Teaches::insert($teaches);}
-  // saving the courses of teachers
- 
-
+  exit();
 }
-Enrollment::enroll();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -48,103 +50,106 @@ Enrollment::enroll();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../assets/css/TeacherRegister.css">
-  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">  
+  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
   <title>Kemet Sign Up</title>
 </head>
 
 <body>
 
-  <div class="container" >
+  <div class="container">
     <div class="head">
       <h2>SignUp</h2>
     </div>
-    <div class="body" >
+    <div class="body">
       <form method="Post">
         <input type="text" name="userName" class="user-name" placeholder="UserName"><br>
-        <?php echo $arr["userName"] ?? " "; echo $arr["account"] ?? " ";?>
+        <?php echo $arr["username"] ?? " ";
+        echo $arr["account"] ?? " "; ?>
         <input type="text" name="name" class="name" placeholder="name"><br>
-        <?php echo $arr["name"] ?? " ";?>
+        <?php echo $arr["name"] ?? " "; ?>
         <input type="password" name="password" class="password" placeholder="Password"><br>
-        <?php echo $arr["password"] ?? " ";?>
+        <?php echo $arr["password"] ?? " "; ?>
         <input type="text" name="address" class="address" placeholder="address"><br>
-        <?php echo $arr["address"] ?? " ";?>
+        <?php echo $arr["address"] ?? " "; ?>
 
         <div class="dropdown">
           <button>Choose courses<i class="uil uil-angle-down"></i></button>
-          
+
           <div class="content">
 
-          <label>Electronics<input type="checkbox" name="checkbox"/></label>
-           
-          <label>Math-1 <input type="checkbox" name="checkbox"/> </label>
+            <label>Electronics<input type="checkbox" name="course[]" value="Electronics"></label>
 
-            <label>Math-0<input type="checkbox"name="checkbox"/></label>
+            <label>Math-1 <input type="checkbox" name="course[]" value="Math-1" /> </label>
 
-              <label>Human Rights<input type="checkbox"name="checkbox"/></label>
+            <label>Math-0<input type="checkbox" name="course[]" value="Math-0" /></label>
 
-                <label>Technical Report<input type="checkbox"name="checkbox"/></label>
+            <label>Human Rights<input type="checkbox" name="course[]" value="Human Rights" /></label>
 
-                  <label>Discrete Math<input type="checkbox"name="checkbox"/></label>
+            <label>Technical Report<input type="checkbox" name="course[]" value="Technical Report" /></label>
 
-                    <label>Programming 1<input type="checkbox"name="checkbox"/></label>
+            <label>Discrete Math<input type="checkbox" name="course[]" value="Discrete Math" /></label>
 
-                      <label>Logic Design<input type="checkbox"name="checkbox"/></label>
+            <label>Programming 1<input type="checkbox" name="course[]" value="Programming 1" /></label>
 
-                        <label>Math-2<input type="checkbox"name="checkbox"/></label>
+            <label>Logic Design<input type="checkbox" name="course[]" value="Logic Design" /></label>
 
-                          <label>Probability and Statistics 1<input type="checkbox"name="checkbox"/></label>
+            <label>Math-2<input type="checkbox" name="course[]" value="Math-2" /></label>
 
-                            <label>Microeconomics<input type="checkbox"name="checkbox"/></label>
+            <label>Probability and Statistics 1<input type="checkbox" name="course[]"
+                value="Probability and Statistics 1" /></label>
 
-                              <label>Introduction to Network<input type="checkbox"name="checkbox"/></label>
+            <label>Microeconomics<input type="checkbox" name="course[]" value="Microeconomics" /></label>
 
-                                <label>Introduction to Database<input type="checkbox"name="checkbox"/></label>
+            <label>Introduction to Network<input type="checkbox" name="course[]"
+                value="Introduction to Network" /></label>
 
-                                  <label>Introduction to Software Engineering<input type="checkbox"name="checkbox"/></label>
+            <label>Introduction to Database<input type="checkbox" name="course[]"
+                value="Introduction to Database" /></label>
 
-                                    <label>Programming 2<input type="checkbox"name="checkbox"/></label>
+            <label>Introduction to Software Engineering<input type="checkbox" name="course[]"
+                value="Introduction to Software Engineering" /></label>
 
-                                      <label>Math-3<input type="checkbox"name="checkbox"/></label>
+            <label>Programming 2<input type="checkbox" name="course[]" value="Programming 2" /></label>
 
-                                        <label>Probability and Statistics 2<input type="checkbox"name="checkbox"/></label>
+            <label>Math-3<input type="checkbox" name="course[]" value="Math-3" /></label>
 
-                                          <label>Data Structures<input type="checkbox"name="checkbox"/></label>
+            <label>Probability and Statistics 2<input type="checkbox" name="course[]"
+                value="Probability and Statistics 2" /></label>
 
-                                            <label>Web<input type="checkbox"name="checkbox"/></label>
+            <label>Data Structures<input type="checkbox" name="course[]" value="Data Structures" /></label>
 
-                                              <label>Machine Learning<input type="checkbox"name="checkbox"/></label>
+            <label>Web<input type="checkbox" name="course[]" value="Web" /></label>
 
-                                                <label>Introduction to Operation Research<input type="checkbox"name="checkbox"/></label>
+            <label>Machine Learning<input type="checkbox" name="course[]" value="Machine Learning" /></label>
 
-            <label>Network Labs<input type="checkbox"name="checkbox"/></label>
+            <label>Introduction to Operation Research<input type="checkbox" name="course[]"
+                value="Introduction to Operation Research" /></label>
 
-              <label>Enterpreneurship<input type="checkbox"name="checkbox"/></label>
+            <label>Network Labs<input type="checkbox" name="course[]" value="Network Labs" /></label>
+
+            <label>Enterpreneurship<input type="checkbox" name="course[]" value="Enterpreneurship" /></label>
 
           </div>
-          
-       
-            
-          
-            </div>
-          </div> <?php echo $arr["courses"] ?? " ";?>
-          <div class="invalid-courses hide"> </div><br>
-          <input id="register" type="submit" name="register" value="Register"><br>
-          <a class="font-dark" href="login.php">Login</a>
-  
-        </form>
-      </div>
-      <div class="footer" >
-        <p>Powerd by Kemet University</p>
-      </div>
-    </div>
+
         </div>
-        
-       
+    </div>
+    <?php echo $arr["courses"] ?? " "; ?>
+    <input id="register" type="submit" name="register" value="Register">
+    <br><br>
+    <a class="font-dark" href="login.php">Login</a>
+
+    </form>
+  </div>
+  <div class="footer">
+    <p>Powerd by Kemet University</p>
+  </div>
+  </div>
+  </div>
+
+
 
 </body>
 
 </html>
 <script type="module" src="../js/TeacherRegister.js?t=<?= time() ?>"></script>
 <script type="module" src="../js/Utils.js?t=<?= time() ?>"></script>
-
-
