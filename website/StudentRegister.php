d813c901eb50b8fac5826393f54e2cc03640f190
EyadEldokany <eyad.eldokany@gmail.com>
2024-04-19 00:36:48 +0200

    update

--- website/StudentRegister.php
+++ website/StudentRegister.php
@@ -1,94 +1,103 @@
 <?php
-require_once ("../config/setup.php");
-require_once ("../models/Accounts.php");
-require_once ("../models/Courses.php");
-require_once ("../models/Enrollment.php");
-require_once ("../models/Students.php");
-require_once ("../models/Teachers.php");
-require_once ("../models/Teaches.php");
-require_once ("../models/Utils.php");
+require_once("../config/setup.php");
+require_once("../models/Accounts.php");
+require_once("../models/Courses.php");
+require_once("../models/Enrollment.php");
+require_once("../models/Students.php");
+require_once("../models/Teachers.php");
+require_once("../models/Teaches.php");
+require_once("../models/Utils.php");
 $arr = [];
 if (isset($_POST["register"])) {
-  $arr = Utils::validateStudentFields([
-    $_POST["userName"],
-    $_POST["name"],
-    $_POST["password"],
-    $_POST["address"],
-    $_POST["level"]
-  ]);
-  $userName = $_POST["userName"];
-  $name = $_POST["name"];
-  $password = $_POST["password"];
-  $address = $_POST["address"];
-  $level = $_POST["level"];
-  if (empty($arr)) {
-    $Account = [$userName, $password];
-    Accounts::insert($Account);
-    $accountId = intval(Accounts::select(["id"], ["userName" => $userName])[0]["id"]);
-    $student = [$accountId, $userName, $name, $address, $level];
-    Students::insert($student);
-    Enrollment::enroll();
-    $_SESSION["id"] = $accountId;
-    $_SESSION["userName"] = $userName;
-    $_SESSION["type"] = "student";
-  }
+    $arr = Utils::validateStudentFields([
+        $_POST["userName"],
+        $_POST["name"],
+        $_POST["password"],
+        $_POST["address"],
+        $_POST["level"]
+    ]);
+    $userName = $_POST["userName"];
+    $name = $_POST["name"];
+    $password = $_POST["password"];
+    $address = $_POST["address"];
+    $level = $_POST["level"];
+    if (empty($arr)) {
+        $Account = [$userName, $password];
+        Accounts::insert($Account);
+        $accountId = intval(Accounts::select(["id"], ["userName" => $userName])[0]["id"]);
+        $student = [$accountId, $userName, $name, $address, $level];
+        Students::insert($student);
+        Enrollment::enroll();
+        $_SESSION["id"] = $accountId;
+        $_SESSION["userName"] = $userName;
+        $_SESSION["type"] = "student";
+    }
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../assets/css/StudentRegister.css">
-  <title>Kemet Sign Up</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Kemet Sign Up</title>
+    <link rel="stylesheet" href="C:\Users\ASUS\Downloads\sis-main (2)\sis-main\assets\css\StudentRegister.css">
 </head>
 
 <body>
-
-  <div class="container">
-    <div class="head">
-      <h2>SignUp</h2>
+    <div class="container">
+        <div class="head">
+            <h2>SignUp</h2>
+        </div>
+        <div class="body">
+            <form id="register">
+                <div>
+                    <input type="text" name="userName" id="user-name" placeholder="UserName" required><br>
+                    <small class="error-msg"></small><br>
+                    <?php
+                    echo $arr["userName"] ?? " ";
+                    echo $arr["account"] ?? " ";
+                    ?>
+                </div>
+                <div>
+                    <input type="text" name="name" id="name" placeholder="Name" required><br>
+                    <small class="error-msg"></small><br>
+                    <?php
+                    echo $arr["name"] ?? " ";
+                    ?>
+                </div>
+                <div>
+                    <input type="password" name="password" id="password" placeholder="Password" required><br>
+                    <small class="error-msg"></small><br>
+                    <?php
+                    echo $arr["password"] ?? " ";
+                    ?>
+                </div>
+                <div>
+                    <input type="text" name="address" id="address" placeholder="Address" required><br>
+                    <small class="error-msg"></small><br>
+                    <?php
+                    echo $arr["address"] ?? " ";
+                    ?>
+                </div>
+                <div>
+                    <input type="text" name="level" id="level" placeholder="Level" required><br>
+                    <small class="error-msg"></small><br>
+                    <?php
+                    echo $arr["level"] ?? " ";
+                    ?>
+                </div>
+                <input type="submit" value="Register">
+                <a class="login" href="index.php">Login</a>
+            </form>
+        </div>
     </div>
-    <div class="body">
-      <form method="Post">
-        <input type="text" name="userName" class="user-name" placeholder="UserName"><br>
-        <?php
-        echo $arr["userName"] ?? " ";
-        echo $arr["account"] ?? " ";
-        ?>
-        <input type="text" name="name" class="name" placeholder="name"><br>
-        <?php
-        echo $arr["name"] ?? " ";
-        ?>
-        <input type="text" name="password" class="password" placeholder="Password"><br>
-        <?php
 
-
-        echo $arr["password"] ?? " ";
-        ?>
-        <input type="text" name="address" class="address" placeholder="address"><br>
-        <?php
-        echo $arr["address"] ?? " ";
-        ?>
-
-        <input type="text" name="level" class="level" placeholder="level"><br>
-        <?php
-
-
-        echo $arr["level"] ?? " ";
-        ?>
-        <input type="submit" name="register" placeholder="Register" value="Regitser"><br>
-        <a class="login" href="index.php">Login</a>
-
-      </form>
+    <div class="footer">
+        <p>Powered by Kemet SIS</p>
     </div>
-  </div>
 
-  <div class="footer">
-    <p>Powerd by Kemet SIS</p>
-  </div>
+    <script src="C:\Users\ASUS\Downloads\sis-main (2)\sis-main\assets\js\signup.js"></script>
 </body>
 
 </html>
-<script type="module" src="../js/StudentRegister.js?t=<?= time() ?>"></script>
