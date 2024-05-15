8e2d0da590ef9aa16133cd50a152ad696c4e8de1
Khemu <alghost2004@gmail.com>
2024-05-16 00:47:56 +0300

    final

--- website/home.php
+++ website/home.php
@@ -22,6 +22,7 @@ if (!$_SESSION["id"]) {
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Home</title>
+  <link rel="icon" type="image/png" href="../assets/icons/laptop.png">
   <link rel="stylesheet" href="../assets/css/home.css" />
   <link rel="stylesheet" href="../assets/css/responsiveHome.css" />
 
